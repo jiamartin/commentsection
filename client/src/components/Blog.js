c164015974abf466eb3556fb9b4c2929d4a17b8c
jiamartin <you@example.com>
2022-04-19 12:59:18 +0800

    blog

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -8,14 +8,16 @@ const Post = ({post}) => {
 
     return (
         <>
-            <Card className='mb-3' style={{ width: '18rem', minHeight: '13rem' }}>
+            <Card className='mb-3' style={{ width: '18rem', minHeight: '15rem', maxHeight: '15rem' }}>
             <Card.Body>
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Text>
                 {ReactHtmlParser(post.body.substring(0, 100))}
                 </Card.Text>
-                <Button onClick={() => console.log(post.id)} className="btn btn-primary" data-toggle="modal">Edit</Button>
             </Card.Body>
+            <Card.Footer>
+            <Button onClick={() => console.log(post.id)} className="btn btn-primary" data-toggle="modal">Edit</Button>
+            </Card.Footer>
             </Card>
 
         </>
